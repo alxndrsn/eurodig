a62f191a55f32f7bf41c4119532081538da46740
Alex Anderson <anderson@medicmobile.org>
2017-03-10 17:45:44 +0545

    Add README

---
+++ README.md
@@ -0,0 +1,11 @@
+# Supported scripts
+
+## [Devanagari](https://en.wikipedia.org/wiki/Devanagari#Numerals)
+
+Commonly used with:
+
+* Hindi
+* Konkani
+* Marathi
+* Nepali
+* Sanskrit
--- test/eurodig.js
+++ test/eurodig.js
@@ -3,7 +3,7 @@ var assert = require('chai').assert;
 describe('eurodig', function() {
 	var eurodig = require('../src/eurodig.js');
 	describe('#tr()', function() {
-		it('should translate Nepali numerals', function() {
+		it('should translate Devanagari numerals', function() {
 			// expect
 			assertTranslation('0 1 2 3 4 5 6 7 8 9',
 			                  '० १ २ ३ ४ ५ ६ ७ ८ ९');
