678492926cec3297a4b68205807848b88c9183ee
Alex Anderson <anderson@medicmobile.org>
2017-04-06 22:09:58 +0200

    Add support for transliterating _to_ western & perso arabic numerals

--- src/to_devanagari.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-/* Magic number: 2406 is the char code for '०' */
-function replacer(c) { return String.fromCharCode(Number(c) + 2406); }
-
-module.exports = function(original) {
-	return original && original.toString().replace(/[0-9]/g, replacer);
-};
